953bae475497f4f56da95677767bf3ac967fd0eb
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-08-22 18:37:05 +0530

    N cents of Coins

---
+++ 22-08-22 Program3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day5c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 2, 3 };
+            int m = arr.Length;
+            Console.Write("Possible ways are: ");
+            Console.Write(count(arr, m, 4));
+            Console.ReadLine();
+        }
+        static int count(int[] S, int m, int n)
+        {
+            if (n == 0)
+                return 1;
+            if (n < 0)
+                return 0;
+            if (m <= 0)
+                return 0;
+            return count(S, m - 1, n) +
+                count(S, m, n - S[m - 1]);
+        }
+    }
+}
