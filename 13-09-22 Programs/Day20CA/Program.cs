ffb68e2318f5ff7f42a263168542fd3c0f2a80ed
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-09-14 00:25:29 +0530

    GAC, Movie Rental System Demo

---
+++ 13-09-22 Programs/Day20CA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Day20GAClib;
+
+namespace Day20CA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Products products = new Products();
+            products.CalculatePrice();
+            products.OfferPrice();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 13-09-22 Programs/Day20GAClib/Products.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20GAClib
+{
+    public delegate double PriceCalculation();
+
+    public class Products
+    {
+        public int Prodid { get; set; }
+        public string Prodname { get; set; }
+        public int Price { get; set; }
+        public int Qty { get; set; }
+
+        public double OfferPrice()
+        {
+            double amt = Qty * Price;
+            double bdis = amt;
+            double dis = amt * .10;
+            double actualPrice = bdis - dis;
+            return actualPrice;
+
+        }
+
+
+        public double CalculatePrice()
+        {
+            return Qty * Price;
+
+        }
+
+    }
+}
