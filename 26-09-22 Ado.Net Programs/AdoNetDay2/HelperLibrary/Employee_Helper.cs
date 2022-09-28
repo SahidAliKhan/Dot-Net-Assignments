6bdc0bda67e2fb1c25b0e842641fd65cf69139ae
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-09-28 23:59:09 +0530

    Ado.Net - CRUD Operations in Connected Architecture

---
+++ 26-09-22 Ado.Net Programs/AdoNetDay2/HelperLibrary/Employee_Helper.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogicLayerLibrary;
+using System.Collections.Generic;
+using DataAccessLayerLibrary;
+namespace HelperLibrary
+{
+    public class Employee_Helper
+    {
+        Employee_DAL dal = null;
+        public Employee_Helper()
+        {
+            dal = new Employee_DAL();
+        }
+        public bool AddEmployee(Employee_BAL employee)
+        {
+            return dal.InsertEmployee(employee);
+        }
+        public bool EditEmployee(Employee_BAL employee)
+        {
+            return dal.UpdateEmployee(employee);
+        }
+        public bool RemvoeEmployee(int employee_id)
+        {
+            return dal.DeleteEmployee(employee_id);
+        }
+        public Employee_BAL SearchEmployee(int empid)
+        {
+            return dal.FindEmployee(empid);
+        }
+        public void SearchEmployee(int empid, out Employee_BAL empdata)
+        {
+
+            dal.FindEmployee(empid, out empdata);
+        }
+        public int countEmployees()
+        {
+            return dal.EmployeeCount();
+
+        }
+        public List<Employee_BAL> ShowEmployeeList()
+        {
+            return dal.EmployeeList();
+        }
+    }
+}
---
+++ 26-09-22 Ado.Net Programs/AdoNetDay2/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using HelperLibrary;
+using BusinessLogicLayerLibrary;
+namespace AdoNetDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Management System");
+            Console.WriteLine("--------------------------------");
+            Employee_Helper helper = new Employee_Helper();
+            repeat:
+            Console.WriteLine("Menu");
+            Console.WriteLine("-----");
+            Console.WriteLine("1. Add New Employee \n 2. Update Employee \n 3. Find Employee \n 4. Show All \n 5.Delete \n  6. Employee count \n 7. Exit ");
+            int userchoice = Convert.ToInt32(Console.ReadLine());
+            Employee_BAL emp = new Employee_BAL();
+            switch (userchoice)
+            {
+                case 1:
+
+                    Console.WriteLine("Enter firstname");
+                    emp.FirstName = Console.ReadLine().Trim();
+
+                    Console.WriteLine("Enter lastname");
+                    emp.LastName = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter title");
+                    emp.Title = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter birthdate");
+                    emp.BirthDate = Convert.ToDateTime(Console.ReadLine());
+
+                    bool queryStatus = helper.AddEmployee(emp);
+                    if (queryStatus)
+                    {
+                        Console.WriteLine("Employee added successfully.....");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Some error occured...");
+                    }
+                    break;
+                case 2:
+
+                    Console.WriteLine("Enter Employeeid ");
+                    emp.EmployeeID = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter firstname");
+                    emp.FirstName = Console.ReadLine().Trim();
+
+                    Console.WriteLine("Enter lastname");
+                    emp.LastName = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter title");
+                    emp.Title = Console.ReadLine().Trim();
+                    Console.WriteLine("Enter birthdate");
+                    emp.BirthDate = Convert.ToDateTime(Console.ReadLine());
+
+                    queryStatus = helper.EditEmployee(emp);
+                    if (queryStatus)
+                    {
+                        Console.WriteLine("Employee updated successfully.....");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Some error occured...");
+                    }
+
+                    break;
+                case 3:
+                    //WithoutOutParameter(helper, emp);
+                    WithOutParameter(helper, emp);
+
+                    break;
+                case 4:
+                    List<Employee_BAL> list = new List<Employee_BAL>();
+                    list = helper.ShowEmployeeList();
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item.EmployeeID);
+                        Console.WriteLine(item.FirstName);
+                        Console.WriteLine(item.LastName);
+                        Console.WriteLine(item.Title);
+                        Console.WriteLine(item.BirthDate);
+                        Console.WriteLine();
+                    }
+                    break;
+                case 5:
+
+                    Console.WriteLine("Enter Employeeid ");
+                    emp.EmployeeID = Convert.ToInt32(Console.ReadLine());
+                    queryStatus = helper.RemvoeEmployee(emp.EmployeeID);
+                    if (queryStatus)
+                    {
+                        Console.WriteLine("Employee deleted successfully.....");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Some error occured...");
+                    }
+
+                    break;
+                case 6:
+                    int cnt = helper.countEmployees();
+                    Console.WriteLine("Total employees=" + cnt);
+                    break;
+
+                case 7:
+                    Environment.Exit(1);
+                    break;
+            }
+            goto repeat;
+            Console.ReadLine();
+        }
+
+        private static void WithOutParameter(Employee_Helper helper, Employee_BAL emp)
+        {
+            Console.WriteLine("Enter Employeeid ");
+            emp.EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Employee_BAL bal = new Employee_BAL();
+            helper.SearchEmployee(emp.EmployeeID, out bal);
+            if (bal != null)
+            {
+                bal.EmployeeID = emp.EmployeeID;
+                Console.WriteLine("following were the details found in the db");
+                Console.WriteLine(bal.EmployeeID);
+                Console.WriteLine(bal.FirstName);
+                Console.WriteLine(bal.LastName);
+                Console.WriteLine(bal.Title);
+                Console.WriteLine(bal.BirthDate);
+            }
+            else
+            {
+                Console.WriteLine("empid not found");
+            }
+        }
+        private static void WithoutOutParameter(Employee_Helper helper, Employee_BAL emp)
+        {
+            Console.WriteLine("Enter Employeeid ");
+            emp.EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Employee_BAL bal = new Employee_BAL();
+            bal = helper.SearchEmployee(emp.EmployeeID);
+            if (bal != null)
+            {
+                bal.EmployeeID = emp.EmployeeID;
+                Console.WriteLine("following were the details found in the db");
+                Console.WriteLine(bal.EmployeeID);
+                Console.WriteLine(bal.FirstName);
+                Console.WriteLine(bal.LastName);
+                Console.WriteLine(bal.Title);
+                Console.WriteLine(bal.BirthDate);
+            }
+            else
+            {
+                Console.WriteLine("empid not found");
+            }
+        }
+    }
+}
