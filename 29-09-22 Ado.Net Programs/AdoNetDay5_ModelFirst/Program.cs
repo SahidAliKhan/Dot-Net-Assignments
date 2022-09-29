dd7bc937d69b662e7a3b93b1f58e0b2ec97dd195
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-09-29 17:50:36 +0530

    Entity Framework - Code First approach and Model First Approach

---
+++ 29-09-22 Ado.Net Programs/AdoNetDay5_ModelFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDay5_ModelFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
