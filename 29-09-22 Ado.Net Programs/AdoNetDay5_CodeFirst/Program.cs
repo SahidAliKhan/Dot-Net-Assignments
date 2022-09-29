e764ec26e6c731d3d8c1173990941f81ff91b940
Sahid Ali Khan <78439917+SahidAliKhan@users.noreply.github.com>
2022-09-29 17:48:41 +0530

    Entity Framework - Code First approach and Model First Approach

---
+++ 29-09-22 Ado.Net Programs/AdoNetDay5_CodeFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityDAL;
+
+namespace AdoNetDay5_CodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+            //context.StudentsTable.Add(new Student { Rollno = 1, Studname = "Amit", City = "Pune", DOB = new DateTime(2022, 11, 11) });
+            //context.StudentsTable.Add(new Student { Rollno = 2, Studname = "Aman", City = "Mumbai", DOB = new DateTime(2002, 08, 04) });
+            //context.StudentsTable.Add(new Student { Rollno = 3, Studname = "Basu", City = "RKL", DOB = new DateTime(2010, 12, 01) });
+
+            //context.CoursesTable.Add(new Course { CourseID = 1, CourseName = "Java", Fees = 10050 });
+
+            context.EnrollmentsTable.Add(new Enrollments { EnrollmentID = 1, CourseID = 1, Rollno = 2 });
+            context.SaveChanges();
+            Console.WriteLine("done");
+            Console.Read();
+        }
+    }
+}
